d8c4bded5b75f2e34cce07acdb246749f1f3f5c4
Gavin Fynbo <fynbo@bu.edu>
2018-03-26 17:15:43 -0400

    remove api key

--- MyPantry/Pantry/views.py
+++ MyPantry/Pantry/views.py
@@ -17,7 +17,7 @@ def index(request):
         # check whether it's valid:
         if form.is_valid():
             query = form.cleaned_data['query']
-            response = requests.get('http://api.yummly.com/v1/api/recipes?&_app_id=09a314ee&_app_key=397b29968b12c73d65e17a07deff9f3e&q=' + query)
+            response = requests.get('http://api.yummly.com/v1/api/recipes?&_app_id=#API_ID_HERE&_app_key=#API_KEY_HERE&q=' + query)
             pantrydata = response.json()
             return render(
                 request,
