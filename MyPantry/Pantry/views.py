e3e2f3fb32354917e4f96be5c2f2353c5f345786
Gavin Fynbo <gavfynbo@gmail.com>
2018-03-26 16:44:00 -0400

    add templates

--- MyPantry/Pantry/views.py
+++ MyPantry/Pantry/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect, render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
+
+from django.conf import settings
+
+import requests
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Pantry index.")
+    response = requests.get('http://api.yummly.com/v1/api/recipes?&_app_id=09a314ee&_app_key=397b29968b12c73d65e17a07deff9f3e&q=salt&excludedIngredient=pepper')
+    pantrydata = response.json()
+    return render(
+        request,
+        'index.html',
+        {
+        'matches': pantrydata['matches']
+    })
