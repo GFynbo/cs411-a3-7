55e9a968e4fa6be3f58c3c72faaee7f2451f6348
Gavin Fynbo <gavfynbo@gmail.com>
2018-03-26 17:56:54 -0400

    update return values

--- MyPantry/Pantry/views.py
+++ MyPantry/Pantry/views.py
@@ -17,14 +17,14 @@ def index(request):
         # check whether it's valid:
         if form.is_valid():
             query = form.cleaned_data['query']
-            response = requests.get('http://api.yummly.com/v1/api/recipes?&_app_id=09a314ee&_app_key=397b29968b12c73d65e17a07deff9f3e&q=' + query)
+            response = requests.get('http://api.yummly.com/v1/api/recipes?&_app_id=ID_HERE&_app_key=KEY_HERE&q=' + query)
             pantrydata = response.json()
             return render(
                 request,
                 'index.html',
-                {
-                'query': pantrydata['criteria']['q']
-            })
+                {'query': pantrydata['criteria']['q'], 'matches': pantrydata['matches']},
+                {'form': form}
+            )
     # if a GET (or any other method) we'll create a blank form
     else:
         form = RecipeSearchForm()
